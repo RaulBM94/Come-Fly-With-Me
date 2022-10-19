f051a376de1e8f6a475b1f25c514d11b408fbe69
Fer Martin <fer@reboot.academy>
2022-10-19 12:49:36 +0100

    refactor

--- src/script.js
+++ src/script.js
@@ -1,161 +1,208 @@
-//Declaración de variables
-const bird = {
-    x: 100,
-    y: 185,
-    direction: null,
-    sprite: document.querySelector('#bird')
+var score = 0
+
+
+/*
+ * BIRD
+ * -- Manejo de nuestro personaje
+ */
+function Bird() {
+  this.x= 100
+  this.y= 185
+  this.direction= null
+  this.sprite= document.querySelector('#bird')
+  this.sprite.style.top = this.y + 'px'
+  this.sprite.style.left = this.x + 'px'
+
+  this.move = function() {
+    // pajaro hacia arriba
+    if (this.direction === 'up' && this.y >= 40) {
+      this.y -= 10
+      this.sprite.style.top = this.y + 'px'
+     }
+    //pajaro hacia debajo
+    if (this.direction === 'down' && this.y <= 300) {
+      this.y += 10
+      this.sprite.style.top = this.y + 'px'
+    }
+  }
 }
-bird.sprite.style.top = bird.y + 'px'
-bird.sprite.style.left = bird.x + 'px'
+let bird = new Bird()
 
-var enemies = []
+document.addEventListener('keydown', function (e) {
+  switch (e.code) {
+    case 'ArrowUp':
+      bird.direction = 'up';
+      break;
+    case 'ArrowDown':
+      bird.direction = 'down';
+      break;
+  }
+})
+document.addEventListener('keyup', function (e) {
+  if (e.code === 'ArrowUp' || e.code === 'ArrowDown') {
+    bird.direction = null
+  }
+})
 
-function enemyStyle() {
-    enemies.forEach(function (enemy) {
-        enemy.sprite.style.top = enemy.y + 'px'
-        enemy.sprite.style.left = enemy.x + 'px'
+/*
+ * ENEMIES
+ * -- Array de enemigos
+ */
+function Enemies() {
+  this.blueBirds = []
+
+  this.addEnemy = () => {
+    const newEnemy = new Enemy()
+    this.blueBirds.push(newEnemy)
+    gameBoard.appendChild(newEnemy.sprite)
+  }
+
+  this.checkCollisionWithBird = function() {
+    let collisions = false
+    for (let i=0; i<this.blueBirds.length; i++) {
+      if (this.blueBirds[i].checkCollision()) {
+        return true
+      }
+    }
+    return false;
+  }
+
+  this.move = function() {
+    this.blueBirds.forEach(enemy => {
+      enemy.move()
     })
-}
+  }
 
+  this.removeAll = function() {
+    this.blueBirds.forEach(function (enemy) {
+      gameBoard.removeChild(enemy.sprite)
+    })
+
+    this.blueBirds = []
+  }
+}
+const enemies = new Enemies()
+
+
+/*
+ * ENEMY
+ * -- Genera un enemigo
+ */
+function Enemy() {
+  this.x = 535;
+  this.y = Math.floor(Math.random() * 340);
+
+  var newEnemy = document.createElement('div');
+  newEnemy.classList.add("enemy")
+  newEnemy.style.top = this.y + 'px'
+
+  this.sprite = newEnemy
+
+  this.move = function () {
+    if (this.x >= -57) {
+      this.x --
+      this.sprite.style.left = this.x + 'px'
+    } else {
+      gameBoard.removeChild(this.sprite)
+      enemies.blueBirds.shift()
+      score++
+      document.getElementById('score').innerHTML = score
+    }
+  }
+
+  // is Enemy collisioning with bird?
+  this.checkCollision = function() {
+    return (
+      this.x < bird.x + 40 &&
+      this.y < bird.y + 30 &&
+      this.x + 45 > bird.x &&
+      this.y + 50 > bird.y)
+  }
+
+  this.move()
+}
 
 var gameBoard = document.getElementById('gameboard')
-var start_timer = 0
-var timer2 = 0
-var inter=0
-var timer3=0
-let [milliseconds, seconds, minutes] = [0, 0, 0];
-var dodge = 0
-let timerRef = document.querySelector('.timerDisplay');
-var score = document.getElementById('score');
 var level = document.querySelector('#level');
 
-//funciones
 
-function spawnEnemy() {//generación del enemigo
-    var newEnemy = document.createElement('div');
-    newEnemy.classList.add("enemy")
-    var enemyObj = {
-        x: 535,
-        y: Math.floor(Math.random() * 340),
-        sprite: newEnemy
-    }
-    enemies.push(enemyObj)
-    enemyStyle()
-    gameBoard.appendChild(newEnemy)
+function gameOver() {
+
+  // Paramos todos los timers
+  clearInterval(game_timerId)
+  clearInterval(enemies_timerId)
+  clearInterval(timer_timerId)
+
+  alert(`Game Over`)
+  restart()
 }
 
-function start() {//inicio juego
-    start_timer = setInterval(function () {
-        move()
-        moveEnemy()
-    }, 100)
-    
-}
-timer2 = setInterval(spawnEnemy(), 3000)
-inter = setInterval(displayTimer(), 10);
-timer3= setInterval(start(),100)//llamada a la función de inicio
-
-function move() {
-    if (bird.direction === 'up' && bird.y >= 40) { moveUp() }//pajaro hacia arriba
-    if (bird.direction === 'down' && bird.y <= 300) { moveDown() }//pajaro hacia debajo
-    scoreCounter()
-    enemies.forEach(function (enemy) {
-        if (enemy.x < bird.x + 40 &&//detección de colisión
-            enemy.y < bird.y + 30 &&//
-            enemy.x + 45 > bird.x &&//
-            enemy.y + 50 > bird.y) {//
-            alert(`Game Over`)
-            clearInterval(timerId)
-            clearInterval(timer2)
-            restart()
-            clearInterval(inter);
-            [milliseconds, seconds, minutes] = [0, 0, 0];
-            timerRef.innerHTML = '00 : 00 : 000 ';
-            dodge = 0
-        }
-    })
-
-}
-
-
-function moveUp() {
-    bird.y -= 10
-    bird.sprite.style.top = bird.y + 'px'
-}
-
-function moveDown() {
-    bird.y += 10
-    bird.sprite.style.top = bird.y + 'px'
-}
-
-function moveEnemy() {
-    enemies.forEach(function (enemy) {
-        if (enemy.x >= 5) {
-            enemy.x -= 5
-            enemy.sprite.style.left = enemy.x + 'px'
-        } else {
-            gameBoard.removeChild(enemy.sprite)
-            enemy.x = 570
-            enemies.shift(enemy)
-        }
-    })
+function checkCollisions() {
+  if (enemies.checkCollisionWithBird()) {
+    gameOver()
+  }
 }
 
 
 function restart() {
-    bird.x = 100
-    bird.y = 185
-    bird.direction = null
-    bird.sprite.style.top = bird.y + 'px'
-    bird.sprite.style.left = bird.x + 'px'
-    var remove = document.querySelectorAll('.enemy')
-    remove.forEach(function(elem){
-        gameBoard.removeChild(elem)
-    })
-    enemies = []
-    clearInterval(timer3)
-    clearInterval(inter)
-    timer_start
-    timer3
-    inter
+  bird = new Bird()
+  enemies.removeAll()
+  resetTimer()
+  score = 0
+  startButton.style= '';
 }
 
-document.addEventListener('keydown', function (e) {
-    switch (e.code) {
-        case 'ArrowUp':
-            bird.direction = 'up'
-            break
-        case 'ArrowDown': bird.direction = 'down'
-            break
-    }
-})
-document.addEventListener('keyup', function (e) {
-    if (e.code === 'ArrowUp' || e.code === 'ArrowDown') {
-        bird.direction = null
-    }
-})
+/*
+ * TIMER
+ * -- displayTimer: Avanza el contador y actualiza el HTML
+ */
+
+let [seconds, minutes] = [0, 0]
+let timerRef = document.querySelector('.timerDisplay')
+function resetTimer() {
+  [seconds, minutes] = [0, 0];
+  timerRef.innerHTML = '00 : 00';
+}
 
 function displayTimer() {
-    milliseconds += 10;
-    if (milliseconds == 1000) {
-        milliseconds = 0;
-        seconds++;
-        if (seconds == 60) {
-            seconds = 0;
-            minutes++;
-        }
-    }
-    let m = minutes < 10 ? "0" + minutes : minutes;
-    let s = seconds < 10 ? "0" + seconds : seconds;
+  seconds++;
+  if (seconds == 60) {
+    seconds = 0;
+    minutes++;
+  }
 
-    timerRef.innerHTML = `${m} : ${s}`;
+  let m = minutes < 10 ? "0" + minutes : minutes;
+  let s = seconds < 10 ? "0" + seconds : seconds;
+
+  timerRef.innerHTML = `${m} : ${s}`;
 }
 
-function scoreCounter() {
-    enemies.forEach(function (enemy) {
-        if (enemy.x === 5) {
-            dodge++
-        }
-    })
-    score.innerHTML = dodge
+
+/*
+ * GAME
+ * -- start: arranca el juego, pajaro, enemigo y cronómetro
+ */
+let game_timerId
+let enemies_timerId
+let timer_timerId
+
+function start() {
+  // Movimiento del pájaro
+  game_timerId = setInterval(function () {
+    bird.move()
+    enemies.move()
+    checkCollisions()
+  }, 20)
+
+  // Generación de enemigos
+  enemies_timerId = setInterval(enemies.addEnemy, 2000)
+
+  // Arranca Cronómetro
+  timer_timerId = setInterval(displayTimer, 1000);
 }
+
+const startButton = document.getElementById('start')
+startButton.addEventListener('click', function () {
+  startButton.style.display = 'none';
+  start();
+})
